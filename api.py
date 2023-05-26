52c6971268c8257d1e7fc599d256eee952893d42
Mosihere <Mostafakhoshhal@yahoo.com>
2023-05-26 16:55:03 +0330

    api info added.

---
+++ api.py
@@ -0,0 +1,23 @@
+API_URL = 'https://www.proxydocker.com/en/api/proxylist/'
+
+
+header = {
+    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:109.0) Gecko/20100101 Firefox/113.0',
+}
+
+data = {
+    'token': 'Mnvc60L-N2qJEGNZdKSNMPILFKg_haqJxDtWC_DdPtk',
+    'country': "all",
+    'city':	"all",
+    'state': "all",
+    'port': "all",
+    'type': "all",
+    'anonymity': "all",
+    'need': "all",
+    'page': "1",
+}
+
+cookies = {
+    'PHPSESSID': "3m1ehv6e8g43bm0dat8cvevn3b",
+}
